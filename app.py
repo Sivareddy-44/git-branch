0e1e416938bbada879dc27feb4179edcb458c974
Sivareddy-44 <yennegivenkatasivareddy44@gmail.com>
2026-09-04 15:07:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+# Get a whole number from the user
+number = int(input("Enter a whole number to check: "))
+
+# Using the modulus operator (%) to check the remainder
+if number % 2 == 0:
+    print(f"{number} is an Even number.")
+else:
+    print(f"{number} is an Odd number.")
